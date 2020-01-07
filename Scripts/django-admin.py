bb91f147610d98e5ff7da16a768f0448e6f8845d
gautamsood15 <gauty22@gmail.com>
2020-01-07 12:32:50 +0530

    Adding project , makemigrations , migrate manage.py

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\gaurav\desktop\projects\url_shortening_service\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
