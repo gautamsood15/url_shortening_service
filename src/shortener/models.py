3cc58e115c1d1c419796b59613163c3ca8af0c14
gautamsood15 <gauty22@gmail.com>
2020-01-14 09:12:08 +0530

    ....

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class shortener(models.Model):
 	url = models.CharField(max_length=220, )
-	#shortcode = models.CharField()
+	shortcode = models.CharField(max_length=15)
 
 	def __str__(self):
 		return str(self.url)
