c91a8462d3717a6b3b39557a9d21fa9cf1773e73
gautamsood15 <gauty22@gmail.com>
2020-01-21 23:14:31 +0530

    Changing dB files

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -4,6 +4,11 @@ from django.db import models
 class shortener(models.Model):
 	url = models.CharField(max_length=220, )
 	shortcode = models.CharField(max_length=15, unique=True)
+	updated = models.DateTimeField(auto_now=True)
+	timestamp = models.DateTimeField(auto_now_add=True)
+	empty_datetime = models.DateTimeField(auto_now=False, auto_new_add=False)
+	
+
 
 	def __str__(self):
 		return str(self.url)
