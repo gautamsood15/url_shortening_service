a68cc43fcf47a29fb40c8701630c410578b77eb3
gautamsood15 <gauty22@gmail.com>
2020-01-22 01:12:11 +0530

    Adding new DB migration

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,13 +1,28 @@
+from random
 from django.db import models
 
 # Create your models here.
+
+
+#def code_generator(size=6 , chars='abcdefghijklmnopqrstuvwxyz'):
+	
+#	return ''.join(random.choice(chars) for _ in range(size))
+
+
 class shortener(models.Model):
 	url = models.CharField(max_length=220, )
 	shortcode = models.CharField(max_length=15, unique=True)
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
-	empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
+	#empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
 	
+
+#	def save(self, *args, **kwargs):
+#		print("something")
+#		super(shortener, self).save(*args , **kwargs)
+
+
+
 
 
 	def __str__(self):
