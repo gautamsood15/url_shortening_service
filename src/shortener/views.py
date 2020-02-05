8cd849b31ef626cc67232fc8bdbd4a082b497c04
gautamsood15 <gauty22@gmail.com>
2020-02-06 04:56:06 +0530

    Adding stuff

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from .models import shortener
@@ -9,7 +9,23 @@ from .models import shortener
 def url_redirect_view(request, shortcode=None, *args, **kwargs):     # FXN based view
 	#print(request.user)
 	#print(request.user.is_authenticated())
-	obj = shortener.objects.get(shortcode=shortcode)
+	#obj = shortener.objects.get(shortcode=shortcode)
+	
+
+	obj = get_object_or_404(shortener, shortcode=shortcode)
+	obj_url = obj.url
+
+	#try:
+	#	obj = shortener.objects.get(shortcode=shortcode)
+	#except:
+	#	obj = shortener.objects.all().first()
+	
+	#obj_url = None	
+	#qs = shortener.objects.filter(shortcode__iexact=shortcode.upper())
+	#if qs.exists() and qs.count() == 1:
+	#	obj = qs.first()
+	#	obj_url = obj.url
+
 	return HttpResponse("hello {sc}".format(sc=obj.url))
 
 
