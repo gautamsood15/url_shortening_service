3f32f3e1a8137a82b3e5c6358b3dcf843bcc38fe
gautamsood15 <gauty22@gmail.com>
2020-02-08 01:54:49 +0530

    Adding stuff

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 
@@ -8,13 +8,13 @@ from .models import shortener
 
 def url_redirect_view(request, shortcode=None, *args, **kwargs):     # FXN based view
 	obj = get_object_or_404(shortener, shortcode=shortcode)
-	return HttpResponse("hello {sc}".format(sc=obj.url))
+	return HttpResponse(obj.url)
 
 
 class URLCBView(View):   # class based view
 	def get(self, request, shortcode=None, *args, **kwargs):
 		obj = get_object_or_404(shortener, shortcode=shortcode)
-		return HttpResponse("hello again  {sc}".format(sc=shortcode))
+		return HttpResponse(obj.url)
 
 
 	def post(self, request, *args, **kwargs):
