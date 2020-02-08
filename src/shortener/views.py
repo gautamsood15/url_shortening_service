29b7f906d74612552411ce291be8f369a95ebdca
gautamsood15 <gauty22@gmail.com>
2020-02-08 16:51:00 +0530

    Adding stuff

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -2,16 +2,39 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 
+from .forms import SubmitUrlForm
 from .models import shortener
 
 # Create your views here.
 
+def home_view_fbv(request, *args, **kwargs):
+	if request.method == "POST":
+		print(request.POST)
+	return render(request, "shortener/home.html", {})
+
+
 class HomeView(View):
 	def get(self, request, *args, **kwargs):
-		return render(request, "home.html", {})
+		the_form = SubmitUrlForm()
+		context = {
+			"title": "Submit URL",
+			"form": the_form
+		}
+		return render(request, "shortener/home.html", context)
 
 
+	def post(self, request, *args, **kwargs):
+		from = SubmitUrlForm(request.POST)
+		if form.is_valid():
+			print(form.cleaned_data)
+		
 
+		context = {
+			"title": "Submit URL",
+			"form": form
+		}
+
+		return render(request, "shortener/home.html", context)
 
 
 
